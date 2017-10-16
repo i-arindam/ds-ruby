6f7f3c71b72e72c109a81580afde0d1ce4a6499c
Arindam Chakraborty <arismart99@gmail.com>
2017-10-16 15:52:02 +0530

    adds functions to get nth node, add after specific node

--- linked_list.rb
+++ linked_list.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Node
   attr_accessor :val, :next
 
@@ -12,10 +13,27 @@ class LinkedList
     @head = Node.new(val, nil)
   end
 
+  def get_nth_node(n)
+    node = @head
+    n -= 1
+    n.times do |i|
+      return "Reached end" unless node
+      node = node.next
+    end
+    node
+  end
+
   def add_at_head(val)
     new_head = Node.new(val, @head.next)
     new_head.next = @head
     @head = new_head
+  end
+
+  def add_after_node(data, prev_node)
+    return "Cannot add to a null node" unless prev_node
+    return "Broken pointer at given node" unless prev_node.next
+    new_node = Node.new(data, prev_node.next)
+    prev_node.next = new_node
   end
 
   def return_list
@@ -34,7 +52,19 @@ ll.add_at_head(20)
 ll.add_at_head(30)
 ll.add_at_head(40)
 
+puts "\n\nTesting simple insert n print \n\n"
 p ll.inspect
+ll.return_list.map do |node|
+  puts node.val
+end
+
+puts "\n\nTesting add_after_node\n\n"
+
+second_node = ll.get_nth_node(2)
+puts "Second node = #{second_node.val}, next = #{second_node.next.val}"
+
+ll.add_after_node(25, second_node)
+
 ll.return_list.map do |node|
   puts node.val
 end
